bb72932e7cb77329f3e4e5bae4516b407e2e4e04
Nathan-Front <jonathantindungan3@gmail.com>
2026-09-01 22:03:39 +0900

    created the design of create account modal

--- script/index.js
+++ script/index.js
@@ -10,6 +10,7 @@ async function fetchHTML() {
   const page = document.body.dataset.page;
   const app = document.getElementById("app");
   const body = document.body;
+  const authContainer = document.querySelector("#auth-modals");
   try {
     app.innerHTML = `
         <div class="loading">
@@ -18,28 +19,35 @@ async function fetchHTML() {
         </div>
     `;
 
-    const [nav, foot, login, userWindow, logoutModal] = await Promise.all([
-      fetch("./components/navigation/nav.html").then((res) => {
-        if (!res.ok) throw new Error("Navigation fetch failed");
-        return res.text();
-      }),
-      fetch("./components/footer/footer.html").then((res) => {
-        if (!res.ok) throw new Error("Footer fetch failed");
-        return res.text();
-      }),
-      fetch("./components/login-create-form/login.html").then((res) => {
-        if (!res.ok) throw new Error("Login form fetch failed");
-        return res.text();
-      }),
-      fetch("./components/login-create-form/userWindow.html").then((res) => {
-        if (!res.ok) throw new Error("User window fetch failed");
-        return res.text();
-      }),
-      fetch("./components/login-create-form/logoutModal.html").then((res) => {
-        if (!res.ok) throw new Error("User window fetch failed");
-        return res.text();
-      }),
-    ]);
+    const [nav, foot, login, userWindow, logoutModal, createAccount] =
+      await Promise.all([
+        fetch("./components/navigation/nav.html").then((res) => {
+          if (!res.ok) throw new Error("Navigation fetch failed");
+          return res.text();
+        }),
+        fetch("./components/footer/footer.html").then((res) => {
+          if (!res.ok) throw new Error("Footer fetch failed");
+          return res.text();
+        }),
+        fetch("./components/login-create-form/login.html").then((res) => {
+          if (!res.ok) throw new Error("Login form fetch failed");
+          return res.text();
+        }),
+        fetch("./components/login-create-form/userWindow.html").then((res) => {
+          if (!res.ok) throw new Error("User window fetch failed");
+          return res.text();
+        }),
+        fetch("./components/login-create-form/logoutModal.html").then((res) => {
+          if (!res.ok) throw new Error("User window fetch failed");
+          return res.text();
+        }),
+        fetch("./components/login-create-form/createAccount.html").then(
+          (res) => {
+            if (!res.ok) throw new Error("User window fetch failed");
+            return res.text();
+          },
+        ),
+      ]);
     let sections = [];
     if (page === "home") {
       sections = await Promise.all([
@@ -58,13 +66,19 @@ async function fetchHTML() {
       ]);
     }
     body.insertAdjacentHTML("beforebegin", nav);
-    body.insertAdjacentHTML("beforeend", login);
+
+    /* body.insertAdjacentHTML("beforeend", login);
     body.insertAdjacentHTML("beforeend", userWindow);
     body.insertAdjacentHTML("beforeend", logoutModal);
+    body.insertAdjacentHTML("beforeend", createAccount); */
     sections.forEach((sec) => {
       app.insertAdjacentHTML("beforebegin", sec);
     });
     body.insertAdjacentHTML("beforeend", foot);
+    authContainer.insertAdjacentHTML(
+      "beforeend",
+      login + userWindow + logoutModal + createAccount,
+    );
     app.innerHTML = "";
   } catch (error) {
     console.log(error);
@@ -100,32 +114,94 @@ export const GOOGLE_APPS_SCRIPT_URL =
 function displayLoginForm() {
   const userBtn = document.querySelectorAll(".user-icon-btn");
   const loginForm = document.querySelector(".login-overlay");
+  const authCon = document.querySelector(".auth-overlay");
   const userData = JSON.parse(localStorage.getItem("userData")) || [];
   userBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
       if (userData) {
         loginForm.classList.add("loginForm");
+        authCon.classList.add("authOpen");
       } else {
         displayUserWindow();
       }
     });
   });
   const closeLoginForm = document.querySelector(".close-login");
+
   closeLoginForm.addEventListener("click", () => {
     loginForm.classList.remove("loginForm");
+    authCon.classList.remove("authOpen");
+  });
+
+  createAccountHandler();
+}
+function loginHandler() {
+  const form = document.querySelector(".login-form");
+  const userInput = document.getElementById("email-input");
+  const passInput = document.getElementById("password-input");
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
   });
 }
+//Create account
+function checkInputs() {
+  const inputs = document.querySelectorAll(".required-create-inputs");
+  const createAccountSubmit = document.querySelector(".create-account-submit");
+  const eulaCheckbox = document.getElementById("eula");
+  const allFilled = [...inputs].every((input) => input.value.trim() !== "");
 
+  if (allFilled) {
+    eulaCheckbox.disabled = false;
+  } else {
+    eulaCheckbox.disabled = true;
+    eulaCheckbox.checked = false;
+    createAccountSubmit.disabled = true;
+  }
+
+  eulaCheckbox.addEventListener("change", () => {
+    createAccountSubmit.disabled = !eulaCheckbox.checked;
+  });
+}
+function createAccountHandler() {
+  const createAccountBtn = document.querySelector(".create-account-btn");
+  const createAccountForm = document.querySelector(".create-account-con");
+  const authCon = document.querySelector(".auth-overlay");
+
+  createAccountBtn.addEventListener("click", () => {
+    authCon.classList.add("authOpen");
+    createAccountForm.classList.add("createAccntForm");
+  });
+
+  const closeCreateAccount = document.querySelector(".close-create-account");
+  closeCreateAccount.addEventListener("click", () => {
+    authCon.classList.remove("authOpen");
+    createAccountForm.classList.remove("createAccntForm");
+  });
+
+  const returnToSignIn = document.querySelector(".return-to-sign-in");
+  returnToSignIn.addEventListener("click", () => {
+    authCon.classList.add("authOpen");
+    createAccountForm.classList.remove("createAccntForm");
+  });
+
+  const form = document.querySelector(".create-account-form");
+  const inputs = form.querySelectorAll(".required-create-inputs");
+  //const allFilled = [...inputs].every((input) => input.value.trim() !== "");
+
+  inputs.forEach((input) => {
+    input.addEventListener("input", checkInputs);
+  });
+}
 //user window
 function displayUserWindow() {
-  const userBtn = document.querySelectorAll(".user-icon-btn");
+  //const userBtn = document.querySelectorAll(".user-icon-btn");
   const userWindow = document.querySelector(".user-window");
 
-  userBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      userWindow.classList.add("userWindow");
-    });
-  });
+  //userBtn.forEach((btn) => {
+  // btn.addEventListener("click", () => {
+  userWindow.classList.toggle("userWindow");
+  //});
+  //});
   displayLogoutModal();
 }
 
